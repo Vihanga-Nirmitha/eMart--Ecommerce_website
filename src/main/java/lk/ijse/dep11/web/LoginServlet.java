2378ebbfe5db1553c56860e065439a88d47fbbee
Vihanga Nirmitha <vihanganirmitha200@gmail.com>
2023-11-11 16:47:22 +0530

    setup login & security filters

---
+++ src/main/java/lk/ijse/dep11/web/LoginServlet.java
@@ -0,0 +1,46 @@
+package lk.ijse.dep11.web;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+        String remember = req.getParameter("remember");
+
+        BasicDataSource pool = (BasicDataSource) getServletContext().getAttribute("connectionpool");
+        try(Connection connection = pool.getConnection()) {
+            PreparedStatement stm = connection.prepareStatement("SELECT username , password FROM user WHERE username=?");
+            stm.setString(1,email);
+           ResultSet rst = stm.executeQuery();
+            if(rst.next()){
+                if(rst.getString("password").equals(password)){
+
+
+                }else{
+                    req.setAttribute("denied", true);
+                    getServletContext().getRequestDispatcher("/signin.jsp").forward(req,resp);
+                }
+
+            }else {
+                req.setAttribute("denied", true);
+                getServletContext().getRequestDispatcher("/signin.jsp").forward(req,resp);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/lk/ijse/dep11/web/filter/SecurityFilter.java
@@ -0,0 +1,26 @@
+package lk.ijse.dep11.web.filter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter("/index.jsp")
+public class SecurityFilter extends HttpFilter {
+
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpSession session = req.getSession(false);
+        if(session != null){
+            chain.doFilter(req,res);
+        }else {
+            res.sendRedirect(req.getContextPath()+"/login.jsp");
+        }
+
+
+    }
+}
