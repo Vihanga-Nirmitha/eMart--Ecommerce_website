7a4cb522042b01866d04596d05a6bd34574d6ce4
Vihanga Nirmitha <vihanganirmitha200@gmail.com>
2023-11-30 09:04:24 +0530

    Update README.md

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 
 Copyright &copy; 2023 DEP-11 <br>
 This project is licensed under this [MIT License](license.txt)
+![Login page](screenshots/Login.png)
+![Home page](screenshots/Dashboard.png)
+![All Products](screenshots/Allproducts.png)
+![Filtered & Sorted Products](screenshots/FilteredProducts.png)
+![Item Preview](screenshots/ItemVIew.png)
+![Cart](screenshots/cart.png)
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,10 +1,10 @@
-CREATE TABLE user(
+CREATE TABLE IF NOT EXISTS user(
       userid VARCHAR(20) primary key ,
       username VARCHAR(100) NOT NULL ,
       password VARCHAR(300) NOT NULL ,
       first_name VARCHAR(100)NOT NULL
 );
-CREATE TABLE item(
+CREATE TABLE IF NOT EXISTS item(
     itemid int AUTO_INCREMENT PRIMARY KEY ,
   title VARCHAR(50) NOT NULL  ,
   category VARCHAR(50) NOT NULL ,
@@ -22,7 +22,7 @@ CREATE TABLE item(
     sold int DEFAULT 0,
     rating DECIMAL(3,1) DEFAULT 5
 );
-CREATE TABLE feedback(
+CREATE TABLE IF NOT EXISTS feedback(
     feedbackid int AUTO_INCREMENT primary key ,
     userid VARCHAR(10) NOT NULL ,
     itemid int NOT NULL ,
@@ -32,7 +32,7 @@ CREATE TABLE feedback(
     CONSTRAINT fk_itemid FOREIGN KEY (itemid) REFERENCES item (itemid)
 
  );
-CREATE TABLE view(
+CREATE TABLE IF NOT EXISTS view(
         viewid int PRIMARY KEY AUTO_INCREMENT,
         itemid int NOT NULL ,
         date DATETIME NOT NULL ,
@@ -40,7 +40,7 @@ CREATE TABLE view(
         cokieid VARCHAR(300) NOT NULL,
         CONSTRAINT fk_item FOREIGN KEY (itemid) REFERENCES item (itemid)
 );
-CREATE TABLE cartitem(
+CREATE TABLE IF NOT EXISTS cartitem(
     cartitemid int PRIMARY KEY AUTO_INCREMENT,
     userid VARCHAR(20) NOT NULL ,
     itemid int NOT NULL ,
@@ -48,26 +48,3 @@ CREATE TABLE cartitem(
     qty int NOT NULL,
     CONSTRAINT fk_user FOREIGN KEY (userid) REFERENCES user (userid)
 );
-INSERT INTO user (userid, username, password, first_name) VALUES ('D000002','vihanganirmitha200@gmail.com','aaaaa','vihanga');
-ALTER table user DROP COLUMN username;
-ALTER TABLE user ADD COLUMN username VARCHAR(100) NOT NULL ;
-SELECT COUNT(*) AS row_count FROM user;
-ALTER  TABLE  item MODIFY COLUMN  rating DECIMAL(3,1) DEFAULT 5 ;
-SELECT * FROM item WHERE price < 20.00 AND price > 5.00 AND (category = 'Electronics' OR category = 'Sports' ) ORDER BY price DESC;
-SELECT * FROM item ORDER BY date DESC;
-
-ALTER TABLE view ADD COLUMN cokieid VARCHAR(300) NOT NULL ;
-SELECT * FROM view WHERE userid = 'D00001';
-SELECT * FROM item as i INNER JOIN (SELECT date,itemid FROM view WHERE userid = 'D00001') as v on v.itemid = i.itemid ORDER BY v.date DESC ;
-
-INSERT INTO feedback (userid, itemid, comment, rating, date) VALUES ('D00001','18','Not Good!' , '3', LOCALTIME);
-SELECT * FROM feedback WHERE itemid = '3';
-SELECT * FROM (SELECT * FROM feedback WHERE itemid = '3') as f INNER JOIN user as u on f.userid = u.userid;
-
-SELECT * FROM item ORDER BY price ASC ;
-SELECT * FROM cartitem WHERE itemid = '3' AND userid = 'D00001';
-SELECT i.itemid,i.price,i.image1,i.title,i.brand,i.model,c.qty  FROM item AS i INNER JOIN cartitem AS c on i.itemid = c.itemid WHERE c.userid = 'D00001';
-UPDATE cartitem SET qty = 5 WHERE userid = 'D00001' AND itemid = '20';
-SELECT i.itemid,i.price,i.image1,i.title,i.brand,i.model,c.qty  FROM item AS i INNER JOIN cartitem AS c on i.itemid = c.itemid WHERE c.userid = 'D00001';
-SELECT * FROM item WHERE title LIKE '%large%'  OR model LIKE '%large%' OR brand LIKE '%large%' OR description LIKE '%large%';
-DELETE FROM cartitem WHERE itemid = 2 AND userid = 'D00001'
